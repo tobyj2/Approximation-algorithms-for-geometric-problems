407ace2d34b018be5423733a3246e42a6f10904c
tobyj2 <43011861+tobyj2@users.noreply.github.com>
2019-02-16 00:17:00 +0100

    Update Octree.java

--- src/Octree.java
+++ src/Octree.java
@@ -5,12 +5,7 @@ import Jcg.geometry.PointCloud_3;
 import Jcg.geometry.Point_3;
 //import Jcg.geometry.Vector_3;
 
-/**
- * A class for representing an Octree
- * 
- * @author Luca Castelli Aleardi, Ecole Polytechnique
- * @version december 2018
- */
+
 public class Octree {
 	public OctreeNode root;
 
